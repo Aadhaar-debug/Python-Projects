a24f0f871a1d8457825fae3b4759eb8bb534fdd7
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-07-07 11:07:19 +0530

    .

---
+++ AGEremaining.py
@@ -0,0 +1,10 @@
+# This program determines how much age are we left with
+age = input("Enter your age")
+age_as_int = int (age)
+
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days {weeks_remaining} weeks and {years_remaining} years ahead of you ")
