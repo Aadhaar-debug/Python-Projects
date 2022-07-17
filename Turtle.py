4b3436418bac3abbea587ca5f408ba9f2bc0298f
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-07-17 05:07:30 -0400

    A simple GUI turtle drawing a shape

---
+++ Turtle.py
@@ -0,0 +1,41 @@
+# importing the required libraries
+import random
+from turtle import Turtle, Screen
+
+# creating the objects
+new = Turtle()
+screen = Screen()
+
+# generating the screen
+print(screen.canvheight)
+
+# generating the turtle object
+print(new.shape("turtle"))
+
+# changing the colour of the turtle
+new.color("red")
+
+# choosing the colours for the walk
+colours = ["red", "black", "blue", "pink", "green"]
+# making th turtle move
+# new.forward(100)
+# new.right(100)
+# new.forward(100)
+# new.right(90)
+# new.forward(200)
+# new.right(90)
+# new.forward(200)
+# new.right(90)
+# new.forward(300)
+# new.right(90)
+# new.forward(300)
+# new.right(90)
+# new.forward(400)
+# new.right(90)
+# new.forward(400)
+# new.right(90)
+# new.forward(500)
+
+
+# clicking the screen to exit
+screen.exitonclick()
