e8fd7a88fbcac78f1d9cb828946dd3770cdc9b9b
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-09-21 10:41:00 +0530

    Activity 3

---
+++ Alphanumeric_or_not.py
@@ -0,0 +1,7 @@
+# program to see if a string is alphanumeric or not
+test_str =  "this is just a test string"
+res =  test_str.isalpha()
+if (res == True):
+    print("the given string is alpha")
+else :
+    print("the given string is not alpha.")
