8068c55d9fe19014a4938b5a831e93ad8ae548f1
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-10-02 14:24:16 -0700

    Activity 5 method 2

---
+++ Activity5_2_Reverse_kth_row.py
@@ -0,0 +1,9 @@
+test_list = [[5,3,2],[8,6,3],[3,5,2],[3,6],[3,7,4],[2,9]]
+
+print("Original list :" , str(test_list))
+
+k = 3
+
+res = [ele[::-1]if(idx+1)%k=0 else ele in enumerate(test_list)]
+
+print("After reversing kth row : " + str(res))
