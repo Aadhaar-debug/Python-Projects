83519b886376f695505823ad04838818d090477e
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-10-12 10:41:16 +0530

    Matrix_multiplicaltion made in python
    This program was a part of the side test that was conducted with the previous test let by Arjun Puri sir.

---
+++ Matrix_multiplication.py
@@ -0,0 +1,24 @@
+# matrix multiplication
+# Program to multiply two matrices using nested loops
+ 
+A = [[12, 7, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+ 
+B = [[5, 8, 1, 2],
+    [6, 7, 3, 0],
+    [4, 5, 9, 1]]
+     
+result = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+ 
+for i in range(len(A)):
+ 
+    for j in range(len(B[0])):
+ 
+        for k in range(len(B)):
+            result[i][j] += A[i][k] * B[k][j]
+ 
+for r in result:
+    print(r)
