882dcbdec915c5b566dd5f0a98d8d46595050b04
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-11-01 20:27:06 -0700

    temperature_converter

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+# Temperature in celsius degree
+celsius = 40
+# Converting the temperature to
+# fehrenheit using the above
+# mentioned formula
+fahrenheit = (celsius * 1.8) + 32
+# printing the result
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit',(celsius,fahrenheit))
