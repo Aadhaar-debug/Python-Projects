7f9885910bd6b02b8dc56b79cff563b8471751af
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2022-11-29 07:18:28 -0800

    Q1 of the lab Experiments

---
+++ TypeChecking_BUiltinfunction.py
@@ -0,0 +1,34 @@
+'''
+QUESTION : 
+Write a program to  demonstrate type checking of the various data types and demonstrate the 
+use of the following built in function in python : abs() , len() , min() , round() , 
+isalnum() , type()
+'''
+
+Number  = 1
+Name  = "ELON MUSK"
+Decimal = 3.14
+Option = True
+x = 1j
+print(type(Name))
+print(type(Number))
+print(type(Decimal))
+print(type(Option))
+print(type(x))
+
+
+print("\n")
+
+'''
+Demonstrating the use of built in functions 
+'''
+
+y = -232
+new = "qwerty"
+list = [5,1,4,2,3]
+Number2 = 7.56477
+
+print(abs(y))
+print(len(new))
+print(min(list))
+print(round(Number2))
