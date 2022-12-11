b4f50a17df5c7b8ad45accfc4e765a0036d3ef5c
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2022-12-11 10:11:24 -0800

    Update AGEremaining.py

--- AGEremaining.py
+++ AGEremaining.py
@@ -8,4 +8,4 @@ days_remaining = years_remaining * 365
 weeks_remaining = years_remaining * 52
 months_remaining = years_remaining * 12
 
-print(f"You have {days_remaining} days {weeks_remaining} weeks and {years_remaining} years ahead of you ")
+print(f"You have {days_remaining} days {weeks_remaining} weeks and {years_remaining} years ahead of you !!!! ")
