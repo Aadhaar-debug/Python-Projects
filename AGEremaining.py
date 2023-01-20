dccfe633213d46b179cfe2665ceb2920dd5ee731
Aadhaar Koul <75715900+Aadhaar-debug@users.noreply.github.com>
2023-01-20 21:09:57 +0530

    Just filling the spaces

--- AGEremaining.py
+++ AGEremaining.py
@@ -2,7 +2,6 @@
 # new project made  aadhaar koul
 age = input("Enter your age")
 age_as_int = int (age)
-
 years_remaining = 90 - age_as_int
 days_remaining = years_remaining * 365
 weeks_remaining = years_remaining * 52
